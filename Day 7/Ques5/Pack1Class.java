66a5c39f1e2d728b8b033c7feeee043da4ef07e1
Anjali <85213285+AnjaliMohta20@users.noreply.github.com>
2022-02-10 15:04:32 +0530

    delete

--- Day 7/Ques5/Pack1Class.java
+++
@@ -1,9 +0,0 @@
-package pack1;
-import mypack1.ProtectedClass;
-public class Pack1Class extends ProtectedClass{
-public static void main(String[] args) {
-	
-	Pack1Class p=new Pack1Class();
-	p.show();
-}
-}
